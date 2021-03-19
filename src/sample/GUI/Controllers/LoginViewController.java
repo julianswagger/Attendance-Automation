ecbe75687d519026ea3661e097d70c4bf84cf5d1
julianswagger <72072509+julianswagger@users.noreply.github.com>
2021-03-19 10:55:35 +0100

    1, 2, 3, 4 how many germans are in my store.
    i know you stealing.

--- src/sample/GUI/Controllers/LoginViewController.java
+++ src/sample/GUI/Controllers/LoginViewController.java
@@ -98,7 +98,7 @@ public class LoginViewController implements Initializable {
                     e.printStackTrace();
                 }
             } else {
-                ErrorLabel.setText("Wrong username or password. Please try again :D");
+                ErrorLabel.setText("Wrong username or password. Please try again");
             }
 
         }
